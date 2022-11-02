bfe56f7bdeacc16136676f6c8e206945406b7377
Sakkar Chowdhury <38989084+SkrChowdhury@users.noreply.github.com>
2022-11-02 23:41:40 +0600

    sign up page, firebase email password authentication done

--- src/Shared/Header/Header.js
+++ src/Shared/Header/Header.js
@@ -5,8 +5,11 @@ import logo from '../../assets/logo.svg';
 const Header = () => {
   const menuItems = (
     <>
-      <li className='font-semibold'>
-        <Link to="/home"> Home</Link>
+      <li className="font-semibold">
+        <Link to="/"> Home</Link>
+      </li>
+      <li className="font-semibold">
+        <Link to="/login"> Login</Link>
       </li>
     </>
   );
